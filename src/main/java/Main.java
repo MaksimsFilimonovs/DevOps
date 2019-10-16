f0380202f84b5329a20ee9a26139c1c9bb9ec875
Maksims <s8_filimo_m@venta.lv>
2019-10-16 23:53:44 +0000

    Added main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		MultiplyConcat test = new MultiplyConcat();
+		System.out.println(test.concatenate("one", "two"));
+		
+		MultiplyConcat test1 = new MultiplyConcat();
+		System.out.println(test1.multiply(2, 3));
+		
+
+	}
+
+}
